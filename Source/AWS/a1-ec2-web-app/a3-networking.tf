f4b53e044d7412315ebbb11cb653de912151c563
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-11 18:54:10 +0530

    Swamy/11oct2022 initial version (#2)
    * Initial Version
    
    * Initial Version
    
    * Networking and Routing Details
    
    * Verified EC2 and Web page

---
+++ Source/AWS/a1-ec2-web-app/a3-networking.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "vpcforwebapp" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpcforwebapp.id
+
+}
+
+resource "aws_subnet" "subnetforwebapp" {
+  cidr_block              = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.vpcforwebapp.id
+  map_public_ip_on_launch = true
+}
---
+++ Source/AWS/a1-ec2-web-app/main.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.34.0"
+    }
+  }
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ssm_parameter" "ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
