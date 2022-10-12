9d4cfb890779f2e2f0c5cfa055fccf3a2897b5af
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-12 12:34:39 +0530

    Swamy/11oct2022 initial version (#3)
    * Initial Version
    
    * Initial Version
    
    * Networking and Routing Details
    
    * Verified EC2 and Web page
    
    * Reorganized the folder
    
    * Initial Version
    
    * Initial Version
    
    * Variables
    
    * Updates to Variables and Locals
    
    * Create outputs.tf
    
    * Renamed files
    
    * Variables for networking section
    
    * Update a5-routing.tf
    
    * Update a6-securitygroup.tf
    
    * Update a7-ec2instance.tf
    
    * Added name for EC2

---
+++ Source/AWS/WebApps/a2-ec2-web-app/a2-locals.tf
@@ -0,0 +1,12 @@
+locals {
+  common_tags = {
+    company      = var.company
+    project      = "${var.company}-${var.project}"
+    billing_code = var.billing_code
+  }
+
+  ec2_name = {
+    Name = "EC2 for ${var.project}"
+  }
+
+}
