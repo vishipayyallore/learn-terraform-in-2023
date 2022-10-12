39f2dd5ddb830be37a204ece16975a70dc40d251
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-12 12:02:08 +0530

    Variables

--- Source/AWS/WebApps/a2-ec2-web-app/a1-variables.tf
+++ Source/AWS/WebApps/a2-ec2-web-app/a1-variables.tf
@@ -3,4 +3,43 @@ variable "region" {
   default = "us-east-1" # North Virginia
 }
 
+variable "enable_dns_hostnames" {
+  type    = bool
+  default = true
+}
 
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_subnet1_cidr_block" {
+  type    = string
+  default = "10.0.0.0/24"
+}
+
+variable "map_public_ip_on_launch" {
+  type    = bool
+  default = true
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "company" {
+  type        = string
+  description = "Company name for resource tagging"
+  default     = "ABC Company"
+}
+
+variable "project" {
+  type        = string
+  description = "Project name for resource tagging"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "Billing code for resource tagging"
+}
