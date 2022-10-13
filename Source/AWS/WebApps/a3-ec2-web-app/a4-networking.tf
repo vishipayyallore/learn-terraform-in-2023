9e5ec93cb985b349307e8fcdf873e0942d629d37
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-13 10:36:14 +0530

    Removed extra "ami" data source

--- Source/AWS/WebApps/a3-ec2-web-app/a4-networking.tf
+++ Source/AWS/WebApps/a3-ec2-web-app/a4-networking.tf
@@ -1,11 +1,3 @@
-data "aws_ssm_parameter" "ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 resource "aws_vpc" "vpcforwebapp" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
@@ -23,6 +15,7 @@ resource "aws_subnet" "subnetforwebapp" {
   cidr_block              = var.vpc_subnetforwebapp_cidr_block
   vpc_id                  = aws_vpc.vpcforwebapp.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = local.common_tags
 }
