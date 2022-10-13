0ae6bf402fed86d87925b78ef4fb10c5e222263b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-13 11:20:48 +0530

    Verified the ALB

--- Source/AWS/WebApps/a3-ec2-web-app/a8-outputs.tf
+++ Source/AWS/WebApps/a3-ec2-web-app/a8-outputs.tf
@@ -1,3 +1,11 @@
 output "aws_instance_public_dns" {
   value = aws_instance.nginx1.public_dns
 }
+
+output "aws_instance2_public_dns" {
+  value = aws_instance.nginx2.public_dns
+}
+
+output "aws_alb_public_dns" {
+  value = aws_lb.nginx.dns_name
+}
