44d7e16ce9a23929aaed526bc9680403c0802ccd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-31 22:26:16 +0530

    Initial Version

---
+++ Source/Local/file-demo1/a2-variables.tf
@@ -0,0 +1,5 @@
+
+variable "games_filename" {
+  type    = string
+  default = "./files/favorite-games.txt"
+}
---
+++ Source/Local/file-demo1/a3-games.tf
@@ -0,0 +1,4 @@
+resource "local_file" "games" {
+  filename = var.games_filename
+  content  = "FIFA 21 | Boxing"
+}
